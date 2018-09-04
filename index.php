b70f8f07e944cab92ea101ec09d6b9a3ee927c5a
cyan <j-l@j-louis.localdomain>
2018-09-04 14:45:53 +0200

    initialisation dossier

---
+++ index.php
@@ -0,0 +1,329 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>MMMoi.Official copie</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Custom styles for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+  <link href="blog.css" rel="stylesheet">
+  <link href="slider.css" rel="stylesheet">
+  
+</head>
+
+<body>
+
+  <div class="container">
+    <header class="py-3">
+      <div class="blog-header row flex-nowrap justify-content-between align-items-center">
+        <div class="col-4">
+          <!-- <a class="text-muted" href="">Subscribe</a> -->
+          <img src="images/square.jpg" width="60" height="60">
+        </div>
+        <div class="col-4 text-center mb-2">
+          <a class="blog-header-logo text-dark">MMMoi</a>
+        </div>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+
+          <a class="btn btn-sm btn-outline-secondary mb-3" href="signin/signin.php">Sign in</a>
+        </div>
+      </div>
+      <div class="nav-scroller py-1  ">
+        <nav class="nav d-flex justify-content-between">
+          <a class="p-2 text-muted" href="#">Home</a>
+          <a class="p-2 text-muted" href="#more">About</a>
+          <a class="p-2 text-muted" href="#artists">Artists</a>
+          <a class="p-2 text-muted" href="#visuals">Visual</a>
+          <a class="p-2 text-muted" href="#">Events</a>
+          <a class="p-2 text-muted" href="#">Contact</a>
+        </nav>
+      </div>
+    </header>
+
+
+    <div class="jumbotron p-3 p-md-4 text-white rounded mmmoipic">
+      <div class="col-md-5 px-0t">
+        <h1  id="fadeInLeft" class="music display-4 font-italic">Music knows no border</h1>
+        <p id="fadeInDown" class="open lead my-3 mt-3">" Open your mind and listen "</p>
+        <p id="more" class="more lead mb-0 "><a href="#more" class="text-white font-weight-bold">Learn more</a></p>
+      </div>
+    </div>
+
+    <div class="about font-italic bg-light"><h3><img src="images/circle2.jpg" width="55px"/> About</h3></div>
+    <div class="text mt-3 p-3  bg-light rounded">
+      <p class="mb-0">In French <strong>Moi</strong> means <strong>Me</strong>... Moi+Moi+Moi=<strong>MMMoi</strong><br><br>
+
+        The triangle of Egocentrism.<br>
+        Narcissism is a term that originated with Narcissus in Greek mythology who fell in love with his own image reflected in a pool of water. Currently it is used to describe a person characterized by egotism, vanity, pride, or selfishness; an erotic gratification derived from admiration of one's own physical or mental attributes.<br> 
+
+        Egocentrism is characterized by preoccupation with one's own internal world. Egocentrics regard themselves and their own opinions or interests as being the most important or valid. To them, self-relevant information is seen to be more important in shaping one’s judgments than are thoughts about others and other-relevant information.Egocentric people are unable to fully understand or to cope with other people's opinions and the fact that reality can be different from what they are ready to accept.<br>
+
+      Egomania is obsessive preoccupation with one's self and applies to someone who follows their own ungoverned impulses and is possessed by delusions of personal greatness and feels a lack of appreciation.Someone suffering from this extreme egocentric focus is an egomaniac. The condition is psychologically abnormal.</p>
+      <!-- <div id="artists"></div> -->
+    </div>
+
+
+    <div id="artists"></div>
+    <div class="artists font-italic bg-light"><h3><img src="images/circle2.jpg" width="55px" /> Artists</h3></div>
+    <div class="dj row mb-2">
+      <div class="col-md-6 mb-1">
+        <div class="card flex-md-row mb-2 shadow-sm h-md-250 bg-light">
+          <div class="card-body d-flex flex-column align-items-start">
+            <!--<strong class="d-inline-block mb-2 text-danger">Artist</strong>-->
+            <h3 class="mb-0 mt-1">
+              <a class="text-dark">Kevin Scherschel</a>
+            </h3>
+            <div class="mb-1 text-muted"><br>Techno<br>DarkTechno<br>Indutrial<br>Ambient</div>
+            <p class="card-text mb-auto mt-2"></p>
+            <a href="#">learn more..</a>
+          </div>
+          <img class="card-img-right flex-auto d-none d-lg-block" src="images/djpics/kevinp2.jpg" alt="Card image cap">
+        </div>
+      </div>
+
+      
+      <div class="col-md-6">
+        <div class="card flex-md-row mb-4 shadow-sm h-md-250 bg-light">
+          <div class="card-body d-flex flex-column align-items-start">
+            <h3 class="mb-0 mt-1">
+              <a class="text-dark" href="#">Oli Fiandrino</a>
+            </h3>
+            <div class="mb-1 text-muted "><br>House<br>Techouse<br>Techno<br>DarkTechno</div>
+            <p class="card-text mb-auto mt-2"></p>
+            <a href="#">learn more...</a>
+          </div>
+          <img class="card-img-right flex-auto d-none d-lg-block" src="images/djpics/oli4.jpg" alt="Card image cap">
+        </div>
+      </div>
+
+
+      <div class="col-md-6">
+        <div class="card flex-md-row mb-4 shadow-sm h-md-250 bg-light">
+          <div class="card-body d-flex flex-column align-items-start">
+            <h3 class="mb-0 mt-1">
+              <a class="text-dark" href="#">Cyan</a>
+            </h3>
+            <div class="mb-1 text-muted"><br>Techno<br>DarkTechno<br>Industrial<br>Ambient</div>
+            <p class="card-text mb-auto mt-2"></p>
+            <a href="#">learn more</a>
+          </div>
+          <img class="card-img-right flex-auto d-none d-lg-block" src="images/djpics/cyan.jpg" alt="Card image cap">
+        </div>
+      </div>
+
+
+      <div class="col-md-6">
+        <div class="card flex-md-row mb-4 shadow-sm h-md-250 bg-light">
+          <div class="card-body d-flex flex-column align-items-start">
+            <h3 class="mb-0">
+              <a class="text-dark" href="#">Kills</a>
+            </h3>
+            <div class="mb-1 text-muted"><br>House<br>Minimal<br>Techno<br>DarkTechno</div>
+            <p class="card-text mb-auto mt-2"></p>
+            <a href="#">learn more</a>
+          </div>
+          <img class="card-img-right flex-auto d-none d-lg-block" src="images/djpics/kills2.jpg" alt="Card image cap">
+        </div>
+        <div id="visuals"></div>
+      </div>
+    </div>
+
+    <!-------SLIDER---------->
+    <div class="about font-italic bg-light"><h3><img src="images/circle2.jpg" width="55px" /> Visual</h3></div>
+    <div class="text mt-3 p-3  bg-light rounded">
+      <p class="mb-0">All our visuals,<br>like videos and flyers are made by a member of our team ....<strong> Jade</strong><br>see some examples of flyers below or click <a href="https://www.youtube.com/watch?v=6zcZ_mpPO6s" target=_blank>here</a></strong> to see videos<br><br></p>
+      
+
+      <div class="slider">
+        <div class="slide_viewer">
+          <div class="slide_group">
+            <div class="slide">
+            </div>
+            <div class="slide">
+            </div>
+            <div class="slide">
+            </div>
+            <div class="slide">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End // .slider -->
+
+
+    <!-----directional nav/btn----->   
+
+    <div class="directional_nav">
+      <div class="previous_btn" title="Previous">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="45px" height="65px" viewBox="-11 -11.5 65 66">
+          <g>
+            <g>
+              <path fill="#474544" d="M-10.5,22.118C-10.5,4.132,4.133-10.5,22.118-10.5S54.736,4.132,54.736,22.118
+              c0,17.985-14.633,32.618-32.618,32.618S-10.5,40.103-10.5,22.118z M-8.288,22.118c0,16.766,13.639,30.406,30.406,30.406 c16.765,0,30.405-13.641,30.405-30.406c0-16.766-13.641-30.406-30.405-30.406C5.35-8.288-8.288,5.352-8.288,22.118z"/>
+              <path fill="#474544" d="M25.43,33.243L14.628,22.429c-0.433-0.432-0.433-1.132,0-1.564L25.43,10.051c0.432-0.432,1.132-0.432,1.563,0 c0.431,0.431,0.431,1.132,0,1.564L16.972,21.647l10.021,10.035c0.432,0.433,0.432,1.134,0,1.564  c-0.215,0.218-0.498,0.323-0.78,0.323C25.929,33.569,25.646,33.464,25.43,33.243z"/>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div class="next_btn" title="Next">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="45px" height="65px" viewBox="-11 -11.5 65 66">
+          <g>
+            <g>
+              <path fill="#474544" d="M22.118,54.736C4.132,54.736-10.5,40.103-10.5,22.118C-10.5,4.132,4.132-10.5,22.118-10.5  c17.985,0,32.618,14.632,32.618,32.618C54.736,40.103,40.103,54.736,22.118,54.736z M22.118-8.288  c-16.765,0-30.406,13.64-30.406,30.406c0,16.766,13.641,30.406,30.406,30.406c16.768,0,30.406-13.641,30.406-30.406 C52.524,5.352,38.885-8.288,22.118-8.288z"/>
+              <path fill="#474544" d="M18.022,33.569c 0.282,0-0.566-0.105-0.781-0.323c-0.432-0.431-0.432-1.132,0-1.564l10.022-10.035      L17.241,11.615c 0.431-0.432-0.431-1.133,0-1.564c0.432-0.432,1.132-0.432,1.564,0l10.803,10.814c0.433,0.432,0.433,1.132,0,1.564 L18.805,33.243C18.59,33.464,18.306,33.569,18.022,33.569z"/>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+    <!-- End // .directional_nav -->
+    
+
+
+    <!--------BLOG------------>
+    <main role="main" class="container">
+      <div class="row">
+        <div class="col-md-8 blog-main">
+          <h3 class="space pb-3 mb-4 font-italic border-bottom">
+            From the Firehose
+          </h3>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Sample blog post</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/r2OTVfm-TUQ?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
+
+            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <hr>
+            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+            <blockquote>
+              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </blockquote>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <h2>Heading</h2>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <h3>Sub-heading</h3>
+            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <pre><code>Example code block</code></pre>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+            <h3>Sub-heading</h3>
+            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <ul>
+              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+              <li>Donec id elit non mi porta gravida at eget metus.</li>
+              <li>Nulla vitae elit libero, a pharetra augue.</li>
+            </ul>
+            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+            <ol>
+              <li>Vestibulum id ligula porta felis euismod semper.</li>
+              <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+              <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
+            </ol>
+            <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+          </div><!-- /.blog-post -->
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Another blog post</h2>
+            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+
+            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+            <blockquote>
+              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </blockquote>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          </div><!-- /.blog-post -->
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">New feature</h2>
+            <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+
+            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <ul>
+              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+              <li>Donec id elit non mi porta gravida at eget metus.</li>
+              <li>Nulla vitae elit libero, a pharetra augue.</li>
+            </ul>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+          </div><!-- /.blog-post -->
+
+          <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Older</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+          </nav>
+
+
+
+        </div>
+        <!-- /.blog-main -->
+        <div class="space p-3">
+          <h4 class="font-italic">Archives</h4>
+          <ol class="list-unstyled mb-0">
+            <li><a href="#">March 2014</a></li>
+            <li><a href="#">February 2014</a></li>
+            <li><a href="#">January 2014</a></li>
+            <li><a href="#">December 2013</a></li>
+            <li><a href="#">November 2013</a></li>
+            <li><a href="#">October 2013</a></li>
+            <li><a href="#">September 2013</a></li>
+            <li><a href="#">August 2013</a></li>
+            <li><a href="#">July 2013</a></li>
+            <li><a href="#">June 2013</a></li>
+            <li><a href="#">May 2013</a></li>
+            <li><a href="#">April 2013</a></li>
+          </ol>
+        </div>
+
+        <div class="p-3">
+          <h4 class="space font-italic">Elsewhere</h4>
+          <ol class="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="https://www.facebook.com/MMMOI.EVENTS" target="_blank">Facebook</a></li>
+          </ol>
+        </div>
+      </aside><!-- /.blog-sidebar -->
+
+    </div><!-- /.row -->
+
+  </main><!-- /.container -->
+
+  <footer class="blog-footer">
+    <div class="col-4 pt-1 mb-3">
+      <a class="text-muted" href="">Sign up to the newsletter</a>
+    </div>
+    <p>MMMoi website built by <a href="https://www.linkedin.com/in/jean-louis-bailet/" target=_blank>Bailet J-Louis</a>.</p>
+    <p>
+      <a href="#">Back to top</a>
+    </p>
+  </footer>
+</div>
+
+    <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script 
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous">
+  </script>
+
+  <script src="slider.js"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+
+
+</body>
+</html>
---
+++ signin/signin.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
+
+    <title>Signin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <div class="home" >
+    <a class="btn2 btn-sm btn-outline-light mb-3" href="../index.php">BACK HOME</a>
+  </div>
+    <form class="form-signin"> 
+      <img class="mt-4 angrytext" src="images/circle4.png" alt="" width="160" height="160">
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label class="remember">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-4 text-muted">&copy; MMMoi</p>
+       <!-- <a class="btn2 btn-sm btn-outline-light mb-3" href="../index.php">BACK HOME</a> -->
+  </body>
+    </form>
+</html>
